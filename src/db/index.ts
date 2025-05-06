ac53af8e3bf8bd8e73a2a8d8d7ac06f187a78a52
Abd Sattout <abdsat@duck.com>
2025-05-06 09:20:59 +0300

    Migrate to Neon

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,8 @@
-import { drizzle } from 'drizzle-orm/vercel-postgres';
-import * as schema from '@/db/schema';
+import * as schema from "@/db/schema";
+import { env } from "@/env";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-const db = drizzle({ schema });
+const db = drizzle({ client: neon(env.DATABASE_URL), schema });
 
 export default db;
